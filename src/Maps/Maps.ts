c2c979510d5330ba25f4527940bc6fb651debb50
Yannis Rohloff <yannis@uni-bremen.de>
2018-01-26 23:36:29 +0100

    fixed cocolinth

--- src/Maps/Maps.ts
+++ src/Maps/Maps.ts
@@ -5,7 +5,7 @@ export class Maps {
     public static imgCocolinth: HTMLImageElement = Maps.image("img/cocolinth.png");
     public static imgCity: HTMLImageElement = Maps.image("img/city.png");
 
-    public static cocolinth = new Map(Maps.imgCocolinth, 16, 16, 100, 100);
+    public static cocolinth = new Map(Maps.imgCocolinth, 16, 16, 160, 128);
     public static city = new Map(Maps.imgCity, 16, 16, 32, 22);
 
     private static image(imagePath: string): HTMLImageElement {
