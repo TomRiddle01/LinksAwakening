c247f42c80edd84c955ed7e0c6687064888b1114
Yannis Rohloff <yannis@uni-bremen.de>
2018-01-28 18:38:53 +0100

    grass only destroyable once

--- src/GameObjects/Grass.ts
+++ src/GameObjects/Grass.ts
@@ -5,15 +5,18 @@ import {StaticTile} from "./StaticTile";
 
 export class Grass extends StaticTile {
 
+    private destroyed = false;
+
     constructor(posX: number, posY: number) {
         super(Sprites.grass, posX, posY);
     }
 
     public collisionWith(object: GameObject) {
-        if (object instanceof DamageHitBox) {
+        if (!this.destroyed && object instanceof DamageHitBox) {
             this.setSprite(Sprites.leaves);
             this.die(this.sprite.getDuration());
             this.playerCollision = false;
+            this.destroyed = true;
         }
     }
 }
